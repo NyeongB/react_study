17c6596ff18aced58f5251717068d63dbe1c13dd
NyeongB <kdtoaj14@ajou.ac.kr>
2022-01-16 10:30:30 +0900

    p130

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,9 +1,9 @@
 import React from "./logo.svg";
-import Say from "./Say";
+import EventPractice from "./EventPractice";
 
 
 function App() {
-  return <Say/>;
+  return <EventPractice/>
 };
 
 export default App;
