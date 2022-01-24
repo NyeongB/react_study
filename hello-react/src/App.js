a02c29e4c947804b363b280f258d7996ce7366f8
NyeongB <kdtoaj14@ajou.ac.kr>
2022-01-24 20:47:12 +0900

    put 'ref' on the component

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,9 +1,18 @@
-import React from "./logo.svg";
-import ValidationSample from "./ValidationSample";
+import React, { Component } from 'react';
+import ScrollBox from "./ScrollBox";
 
 
-function App() {
-  return <ValidationSample/>
-};
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <ScrollBox ref={(ref)=>this.ScrollBox=ref}/>
+        <button onClick={()=> this.ScrollBox.scrollToBottom()}>
+          맨밑으로
+        </button>
+      </div>
+    );
+  }
+}
 
 export default App;
