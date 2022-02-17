05e20e6a92dd4c2cb43e09525427dfe3e8ac9e82
NyeongB <kdtoaj14@ajou.ac.kr>
2022-02-17 21:07:45 +0900

    2.html

---
+++ JS/2.js
@@ -0,0 +1,12 @@
+/*
+        var greeting = function(name){
+            return "hello " + name;
+        };
+
+        console.log(greeting('hi'));
+*/
+        var greeting = (name) => {
+            return 'hello ${name}';
+        };
+
+        console.log(greeting('hi'));
