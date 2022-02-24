bacfb41b817a2ac27d252ba106d485e5b66cb1c3
NyeongB <kdtoaj14@ajou.ac.kr>
2022-02-24 21:43:43 +0900

    3,4,5

---
+++ dream_coding/javascript/operator.js
@@ -0,0 +1,17 @@
+// 1.String concatenation
+
+console.log('my' + ' cat');
+console.log(`string literals: 1 + 2 = ${1+2}`);
+
+// 2. Numeric operators
+
+console.log(1 +1);
+console.log(1 - 1);
+console.log(1 / 1);
+console.log(1 * 1);
+console.log(5 % 2);
+console.log(2 ** 3);
+
+// 3. Increment and decrement operators
+let counter = 2;
+const preIncrement = ++counter;
---
+++ dream_coding/javascript/variable.js
@@ -0,0 +1,15 @@
+'use strict';
+
+// 2. Variable
+// let (added in ES6)
+
+let name = 'ellie';
+console.log(name);
+
+const ccn = {
+    name: 'ccn',
+    age: 30,
+
+}
+
+console.log(ccn);
