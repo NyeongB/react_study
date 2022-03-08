8f5e52854cb9fb963bd2a23f3a94c3b4088a9bb9
NyeongB <kdtoaj14@ajou.ac.kr>
2022-03-08 22:16:41 +0900

    Assignment#2

--- momentum/script.js
+++ momentum/script.js
@@ -1,1 +1,11 @@
-alert('hi');
+const player = {
+    name: "nico",
+    points: 10,
+    fat: true,
+
+};
+
+console.log(player);
+player.lastNmae = "potato";
+player.points = 15;
+console.log(player);
