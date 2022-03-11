c975a6749b44ec9e8255cd83c4c1e8d7b63aedc4
NyeongB <kdtoaj14@ajou.ac.kr>
2022-03-11 19:23:37 +0900

    day5

--- momentum/app.js
+++ momentum/app.js
@@ -1,50 +1,10 @@
 // h1 html element mdn
-const title = document.querySelector("div.hello:first-child h1");
-
-console.log(title);
-console.dir(title);
+const h1 = document.querySelector("h1");
 
 function handleTilteClick() {
-    console.log("title was clicked!");
-    title.style.color = "blue";
-}
+    h1.classList.toggle("clicked");
+};
 
+//h1.onclick = handleTilteClick;
+h1.addEventListener("click", handleTilteClick);
 
-
-
-
-function handleMouseEnter() {
-    title.innerHTML = "Mouse is here!";
-}
-
-function handleMouseLeave() {
-    title.innerHTML = "Mouse is gone!";
-}
-
-// title.addEventListener("click", handleTilteClick);
-title.onclick = handleTilteClick;
-title.addEventListener("mouseenter", handleMouseEnter);
-title.addEventListener("mouseleave", handleMouseLeave);
-
-
-function handleWindowResize() {
-    document.body.style.backgroundColor = "tomato";
-}
-
-window.addEventListener("resize", handleWindowResize);
-
-function handleWindowCopy() {
-    alert("copier!");
-}
-
-function handelWindowOffline() {
-    alert("SOS no WIFI");
-}
-
-function handelWindowOnline() {
-    alert("Oh good WIFI");
-}
-
-window.addEventListener("copy", handleWindowCopy);
-window.addEventListener("offline", handelWindowOffline);
-window.addEventListener("online", handelWindowOnline);
