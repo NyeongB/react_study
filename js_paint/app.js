0491dbd911ed5f063bad8c24c290931eec5f1e4b
NyeongB <kdtoaj14@ajou.ac.kr>
2022-03-14 22:54:03 +0900

    js_paint#2.0

---
+++ js_paint/app.js
@@ -0,0 +1,33 @@
+const canvas = document.getElementById("jsCanvas");
+
+
+let painting = false;
+
+function stopPaingting() {
+    painting = false;
+}
+
+function onMouseMove(event) {
+    
+    const x = event.offsetX;
+    const y = event.offsetY;
+    console.log(x, y);
+
+}
+
+function onMouseDown(event) {
+    painting = true;
+}
+
+function onMouseUp(event) {
+    stopPaingting();
+}
+
+
+
+if(canvas) {
+    canvas.addEventListener("mousemove", onMouseMove);
+    canvas.addEventListener("mousedown", onMouseDown);
+    canvas.addEventListener("mouseup", onMouseUp);
+    canvas.addEventListener("mouseleave", stopPaingting);
+}
