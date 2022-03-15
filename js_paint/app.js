0543b4f9c46b1c0716933fd38dd0c7f2c53ae6ad
NyeongB <kdtoaj14@ajou.ac.kr>
2022-03-15 22:59:05 +0900

    js_paint#2.5

--- js_paint/app.js
+++ js_paint/app.js
@@ -1,17 +1,45 @@
 const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d");
+
+const colors = document.getElementsByClassName("jsColor");
+const range = document.getElementById("jsRange");
+
+const mode = document.getElementById("jsMode");
+
+
+canvas.width = 700;
+canvas.height = 700;
+
+canvas.width = document.getElementsByClassName("canvas")[0].offsetWidth;
+canvas.height = document.getElementsByClassName("canvas")[0].offsetHeight;
+
+ctx.strokeStyle = "#2c2c2c";
+ctx.lineWidth = 2.5;
 
 
 let painting = false;
+let filling = false;
 
 function stopPaingting() {
     painting = false;
+}
+
+function startPainting() {
+    painting = true;
 }
 
 function onMouseMove(event) {
     
     const x = event.offsetX;
     const y = event.offsetY;
-    console.log(x, y);
+    
+    if(!painting){
+        ctx.beginPath(); // 경로 생성
+        ctx.moveTo(x, y); // 선 시작 좌표
+    } else{
+        ctx.lineTo(x,y); // 선 끝 좌표
+        ctx.stroke(); // 선 그리기
+    }
 
 }
 
@@ -19,15 +47,47 @@ function onMouseDown(event) {
     painting = true;
 }
 
-function onMouseUp(event) {
-    stopPaingting();
+function handleColorClick(event) { 
+    
+    const color = event.target.style.backgroundColor;
+    ctx.strokeStyle = color;
+    
 }
 
+function handleRangeChange(event) {
+
+    const size = event.target.value;
+    ctx.lineWidth = size;
+    
+}
+
+function handleModeClick() {
+    if(filling === true){
+        filling = false;
+        mode.innerText = "Fill";
+    } else {
+        filling = true;
+        mode.innerText = "Paint";
+    }
+}
 
 
 if(canvas) {
     canvas.addEventListener("mousemove", onMouseMove);
-    canvas.addEventListener("mousedown", onMouseDown);
-    canvas.addEventListener("mouseup", onMouseUp);
+    canvas.addEventListener("mousedown", startPainting);
+    canvas.addEventListener("mouseup", stopPaingting);
     canvas.addEventListener("mouseleave", stopPaingting);
+}
+
+if(colors) {
+    Array.from(colors).forEach(
+        color => color.addEventListener("click", handleColorClick));
+}
+
+if(range) {
+    range.addEventListener("input", handleRangeChange);
+}
+
+if(mode){
+    mode.addEventListener("click", handleModeClick);
 }
